40a166877f30b3388933a2dc699114e68f137ac1
Peter Huber <p.huber@anfe.ma>
2016-03-21 11:11:16 +0100

    Added error handling for token login, where the property "user" can get null at the first login. This is caused by an ION bug, and should be fixed in future releases

--- AMP/DataModel/Login.cs
+++ AMP/DataModel/Login.cs
@@ -1,11 +1,10 @@
-﻿using System.Net.Http.Headers;
-
-namespace Anfema.Amp.DataModel
+﻿namespace Anfema.Amp.DataModel
 {
     public class Login
     {
         public string token{ get; set; }
-        public int user { get; set; }
+        public int? user { get; set; } // is nullable because of an AMP error...
         public string api_url { get; set; }
+        public string version { get; set; }
     }
 }
