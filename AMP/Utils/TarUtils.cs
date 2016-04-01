f3ec96e11d855f629b6d58ccf49b8d5f8015efde
Henning Eenboom <h.eenboom@anfe.ma>
2016-04-01 08:16:15 +0200

    Re #10 - Archive download
    - Added TarUtils class

---
+++ AMP/Utils/TarUtils.cs
@@ -0,0 +1,39 @@
+﻿using SharpCompress.Reader.Tar;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace Anfema.Amp.Utils
+{
+    public class TarUtils
+    {
+        public async static Task ExtractTarArchiveAsync(Stream archiveStream, StorageFolder destinationFolder)
+        {
+            using (var reader = TarReader.Open(archiveStream))
+            {
+                while (reader.MoveToNextEntry())
+                {
+                    if ( !reader.Entry.IsDirectory )
+                    {
+                        using ( var entryStream = reader.OpenEntryStream() )
+                        {
+                            string fileName = Path.GetFileName(reader.Entry.Key);
+                            string folderName = Path.GetDirectoryName(reader.Entry.Key);
+
+                            var folder = destinationFolder;
+                            if (string.IsNullOrWhiteSpace( folderName ) == false)
+                                folder = await destinationFolder.CreateFolderAsync( folderName, CreationCollisionOption.OpenIfExists );
+
+                            var file = await folder.CreateFileAsync(fileName, CreationCollisionOption.OpenIfExists);
+                            using ( var fileStream = await file.OpenStreamForWriteAsync().ConfigureAwait( false ) )
+                            {
+                                await entryStream.CopyToAsync( fileStream );
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
