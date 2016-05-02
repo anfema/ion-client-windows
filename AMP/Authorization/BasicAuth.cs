14f455e5948dc9b1e7034b5d1030fb0e6971dcf1
Peter Huber <p.huber@anfe.ma>
2016-05-02 17:34:11 +0200

    Made the code cleaner and removed redundant parts Re #1

--- AMP/Authorization/BasicAuth.cs
+++ AMP/Authorization/BasicAuth.cs
@@ -1,24 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http.Headers;
 using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Anfema.Ion.Authorization
 {
     public class BasicAuth
     {
-        // generate base64 encoded header from username and password
+        // Generate base64 encoded header from username and password
         public static AuthenticationHeaderValue GetAuthHeaderValue( string username, string password )
         {
             try
             {
-                String credentials = username + ':' + password;
-                String credentialsBase64 = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(credentials));
+                string credentials = username + ':' + password;
+                string credentialsBase64 = Convert.ToBase64String( Encoding.UTF8.GetBytes( credentials ) );
                 return new AuthenticationHeaderValue( "Basic", credentialsBase64 );
             }
-            catch ( Exception e )
+            catch
             {
                 return null;
             }
